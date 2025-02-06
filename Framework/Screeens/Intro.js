dd7c35cdbd7231f8964329ada834b0489ed10434
Miracle Obafemi <miracleobafemi09@gmail.com>
2025-02-06 16:56:33 +0100

    New Updates

--- Framework/Screeens/Intro.js
+++ Framework/Screeens/Intro.js
@@ -1,18 +1,26 @@
 import { Image, SafeAreaView, ScrollView, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import { AppButton } from '../Components/AppButton';
+import { Theme } from '../Components/Theme';
 
 export function Intro() {
     return (
         <SafeAreaView style={{ flex: 1 }}>
             <View style={styles.container}>
+                <Text style={styles.header}>RentNex</Text>
+                <View style={{ height: 230 }}>
+                    <ScrollView horizontal>
+                        <Image source={require("../../assets/slide1.jpg")} style={styles.img} />
+                        <Image source={require("../../assets/slide2.jpg")} style={styles.img} />
+                        <Image source={require("../../assets/slide3.jpg")} style={styles.img} />
+                    </ScrollView>
+                    <Text style={{}}>RentNex</Text>
+                </View>
 
-                <Text style={styles.header}>RentNex</Text>
-                <ScrollView horizontal style={{}}>
-                    <Image source={require("../../assets/slide1.jpg")} style={styles.img} />
-                    <Image source={require("../../assets/slide2.jpg")} style={styles.img} />
-                    <Image source={require("../../assets/slide3.jpg")} style={styles.img} />
-                </ScrollView>
-                <Text style={styles.header}>RentNex</Text>
+                <View style={{ gap: 10 }}>
+                    <AppButton>Get Started</AppButton>
+                    <AppButton style={{ backgroundColor: "white", borderWidth: 1, borderColor: Theme.colors.primary }} textColor={Theme.colors.primary}>Sign In</AppButton>
+                </View>
             </View>
         </SafeAreaView>
     )
@@ -21,7 +29,8 @@ export function Intro() {
 const styles = StyleSheet.create({
     container: {
         padding: 20,
-        flex: 1
+        flex: 1,
+        justifyContent: "space-between"
     },
     header: {
         fontFamily: "Lobster_400Regular",
