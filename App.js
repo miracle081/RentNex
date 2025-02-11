1fd67e4504ef3fe692e8fa55f5457ae7ead5204d
Miracle Obafemi <miracleobafemi09@gmail.com>
2025-02-11 15:09:49 +0100

    New Updates

--- App.js
+++ App.js
@@ -1,11 +1,50 @@
-import * as React from 'react';
-import { Intro } from './Framework/Screens/Intro';
+import { useEffect, useState, useCallback } from "react";
 import { StackNavigator } from './Framework/Navigation/Stack';
 import { AppProvider } from './Framework/Components/globalVariables';
-
+import {
+  Montserrat_100Thin, Montserrat_200ExtraLight, Montserrat_300Light, Montserrat_400Regular,
+  Montserrat_500Medium, Montserrat_600SemiBold, Montserrat_700Bold, Montserrat_800ExtraBold, Montserrat_900Black
+} from "@expo-google-fonts/montserrat"
+import * as SplashScreen from "expo-splash-screen";
+import * as Font from "expo-font";
+import { Lobster_400Regular } from "@expo-google-fonts/lobster";
 
 export default function App() {
+  const [appIsReady, setAppIsReady] = useState(false);
 
+  useEffect(() => {
+    async function prepare() {
+      try {
+        await Font.loadAsync({ Lobster_400Regular });
+        await Font.loadAsync({ Montserrat_100Thin });
+        await Font.loadAsync({ Montserrat_200ExtraLight });
+        await Font.loadAsync({ Montserrat_300Light });
+        await Font.loadAsync({ Montserrat_400Regular });
+        await Font.loadAsync({ Montserrat_500Medium });
+        await Font.loadAsync({ Montserrat_600SemiBold });
+        await Font.loadAsync({ Montserrat_700Bold });
+        await Font.loadAsync({ Montserrat_800ExtraBold });
+        await Font.loadAsync({ Montserrat_900Black });
+        await new Promise(resolve => setTimeout(resolve, 2000));
+      } catch (e) {
+        console.warn(e);
+      } finally {
+        setAppIsReady(true);
+      }
+    }
+
+    prepare();
+  }, []);
+
+  useCallback(async () => {
+    if (appIsReady) {
+      await SplashScreen.hideAsync();
+    }
+  }, [appIsReady]);
+
+  if (!appIsReady) {
+    return null;
+  }
   return (
     <AppProvider>
       <StackNavigator />
